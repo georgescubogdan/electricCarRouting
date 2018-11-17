1d8f28b79b22c587ee9ec2c3a7e7bc3ca0f59cb7
ICosminel <petregcosmin@gmail.com>
2018-11-18 01:43:25 +0200

    parser

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 import { Router } from '@angular/router';
+import { RestService } from '../http.service';
+import { route } from '../classes';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +11,23 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private loginService : LoginService, private router: Router) { }
+  constructor(private loginService : LoginService, private router: Router, private rest: RestService) { }
 
   ngOnInit() {
+    var routes : route[];
+    routes = this.rest.getRoutesFromString('asa#da#ba%ok1#ok2#ok3');
+    for(let x of routes){
+      console.log("start: ", x.start, " end: ", x.end, " path: ", x.path);
+    }
+
+    var x = this.rest.getStringFromRoutes(routes);
+
+    console.log("text is: " + x);
+
     this.loginService.stateObservable.subscribe(e => {
       //console.log(e);
       if (e === false)
         this.router.navigate(['login']);
     });
   }
-
 }
