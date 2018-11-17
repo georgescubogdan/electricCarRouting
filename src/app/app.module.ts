502f7a74d317dd71f43dbce4a7338e2325aaef5d
ICosminel <petregcosmin@gmail.com>
2018-11-17 14:12:28 +0200

    routing + home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MatIconModule } from '@angular/material';
 import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     MatIconModule,
     MaterialModule
   ],
