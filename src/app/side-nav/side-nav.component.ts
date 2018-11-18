34ec85826b3645ce46d9acd7b35ab83c4aca0b7d
Bogdan Georgescu <georgescubogdan96@gmail.com>
2018-11-18 10:46:47 +0200

    new

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -33,7 +33,7 @@ export class SideNavComponent implements OnInit, AfterViewInit {
   fromPlace: string;
 
   routes : route[];
-  res : result[];
+  res : result[] = [];
   events: string[] = [];
   opened: boolean = true;
   first: String;
@@ -44,6 +44,7 @@ export class SideNavComponent implements OnInit, AfterViewInit {
       fixed: false,
       top: 0
     });
+    this.nearbyPlaces({latitude : 44.439663, longitude: 26.096306});
   }
 
 
@@ -333,6 +334,20 @@ updateUsedChargers(geojson) {
     console.log('maxdet: ', this.valueMaxdet)
   }
 
+  nearbyPlaces(location: Location) {
+    
+    
+    this.http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='
+            + location.latitude + ',' + location.longitude
+            + '&radius=10000&type=museum&key=AIzaSyAyfjHiYM9wOoWxCHaTVv5nabpxoAqaLhM').subscribe(
+            (e : any) => {
+              console.log(e.results);
+              e.results.forEach(element => {
+                this.res.push({name :element.name})
+              });
+      }
+    );
+  }
   signOut(){
     this.amplifyService.auth().signOut();
     this.loginService.signedIn = false;
