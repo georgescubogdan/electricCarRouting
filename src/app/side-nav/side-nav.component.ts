7acc9a00cf766027be4454700f865169f1a7af7e
ICosminel <petregcosmin@gmail.com>
2018-11-18 04:32:28 +0200

    all bindings

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -89,9 +89,62 @@ export class SideNavComponent implements OnInit {
     this.secondCity = location;
   }
 
-  search(){
+  valueHours = '';
+  valueHops = '';
+  valueMaxdist = '';
+  valueMaxdet = '';
+
+  search() {
+    var data = (<HTMLInputElement>document.getElementById("hours"));
+    if(data == null || Number.isNaN(parseInt(data.value, data.value.length))) {
+          this.valueHours = "0";
+    }
+    else {
+      this.valueHours = data.value;
+    }
+
+    data = (<HTMLInputElement>document.getElementById("hops"));
+    if(data == null || Number.isNaN(parseInt(data.value, data.value.length))) {
+          this.valueHops = "0";
+    }
+    else {
+      this.valueHops = data.value;
+    }
+
+    data = (<HTMLInputElement>document.getElementById("maxdist"));
+    if(data == null || Number.isNaN(parseInt(data.value, data.value.length))) {
+          this.valueMaxdist = "0";
+    }
+    else {
+      this.valueMaxdist = data.value;
+    }
+
+    data = (<HTMLInputElement>document.getElementById("maxdet"));
+    if(data == null || Number.isNaN(parseInt(data.value, data.value.length))) {
+          this.valueMaxdet = "0";
+    }
+    else {
+      this.valueMaxdet = data.value;
+    }
+
+    // this.valueHops = (<HTMLInputElement>document.getElementById("hops")).value;
+    // this.valueMaxdist = (<HTMLInputElement>document.getElementById("maxdist")).value;
+    // this.valueMaxdet = (<HTMLInputElement>document.getElementById("maxdet")).value;
+
+    // if(Number.isNaN(parseInt(data, data.length))) {
+    //   this.valueMaxdet = "0";
+    // }
+    // else {
+    //   this.valueMaxdet = data;
+    // }
+
+
     console.log('first: ', this.firstCity.latitude, this.firstCity.longitude);
     console.log('second: ', this.secondCity.latitude, this.secondCity.longitude);
+    console.log('hours: ', this.valueHours);
+    console.log('hops: ', this.valueHops);
+    console.log('max dist: ', this.valueMaxdist);
+    console.log('max det: ', this.valueMaxdet);
   }
 
   signOut(){
@@ -99,8 +152,6 @@ export class SideNavComponent implements OnInit {
     this.loginService.signedIn = false;
     this.loginService.user = null;
   }
-
-  
 
   save() {
     
