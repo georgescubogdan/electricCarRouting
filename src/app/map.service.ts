9733158d27227bd4b54b8caabb04508980a66e5b
impmihai <mihai.lupea@gmail.com>
2018-11-18 09:10:57 +0200

    directions

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,12 +1,111 @@
 import { Injectable } from '@angular/core';
 import * as mapboxgl from '../../node_modules/mapbox-gl/dist/mapbox-gl.js';
 import { Map } from '../../node_modules/mapbox-gl/dist/mapbox-gl.js';
+import { HttpClient } from '@angular/common/http';
+import * as turf from '@turf/turf';
 
 @Injectable()
 export class MapService {
   map: Map<any, any>;
+  nothing = turf.featureCollection([]);
+  
+  constructor(private http: HttpClient) {
+    (mapboxgl as any).accessToken = 'pk.eyJ1Ijoia2V5a2V5a2V5IiwiYSI6ImNqb21janRkajA0bHczcXBmeTV6NTBndTkifQ.K-zjwu3uNA37kZs6fghbVw';
+  }
+  
+  
+  // Here you'll specify all the parameters necessary for requesting a response from the Optimization API
+  assembleQueryURL(points: any[]) {
+    
+    // Store the location of the truck in a variable called coordinates
+    var coordinates: any = points;
+    
+    // Set the profile to `driving`
+    // Coordinates will include the current location of the truck,
+    return 'https://api.mapbox.com/optimized-trips/v1/mapbox/driving/' + coordinates.join(';') + '?overview=full&roundtrip=false&source=first&destination=last&steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+  }
+  
+  //primeste array de latitude, longitude (nu stiu daca asta e ordinea)
+  async getCharginStationsCloseToPoint(point: number[]) {
+    return await this.http.get('https://api.openchargemap.io/v2/poi/?latitude=' + point[1] + '&longitude=' + point[0] + '&distance=300&maxresults=10').toPromise();
+  }
+  
+  //primeste array de array de coordonate => geojson
+  async getRouteWithPoints(points) {
+    let data = await this.http.get(this.assembleQueryURL(points)).toPromise();
+    // Create a GeoJSON feature collection
+    var routeGeoJSON = turf.featureCollection([turf.feature(data['trips'][0].geometry)]);
+    
+    // If there is no route provided, reset
+    if (!data['trips'][0]) {
+      routeGeoJSON = this.nothing;
+    }
+    // Update the `route` source by getting the route source
+    // and setting the data equal to routeGeoJSON
+    // var chunk = turf.lineChunk(routeGeoJSON, 15, {units: 'miles'});
+    
+    return routeGeoJSON;
+  }
+  
+  
+  async makeRoad(from, to) {
+    let data = await this.http.get(this.assembleQueryURL([from, to])).toPromise();
 
-  constructor() {
-    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiYWlweSIsImEiOiJjam9samRwMGMwaGkzM3FvNGM1MTRua2FyIn0.oPAZeJD7PZ4t9nd8Hjl39g';
+    let geoJson: any;
+    var routeGeoJSON = turf.featureCollection([turf.feature(data['trips'][0].geometry)]);
+    let stations = [];
+    let usedStations = [];
+
+    // If there is no route provided, reset
+    if (!data['trips'][0]) {
+      routeGeoJSON = this.nothing;
+    } else {
+      var chunks = turf.lineChunk(routeGeoJSON, 300, {units: 'kilometers'});
+      var waypoints = [from];
+      for (let i = 0; i < chunks.features.length; i ++) {
+        let chunk = chunks.features[i].geometry['coordinates'];
+        let stationsFarEnd: any = await this.getCharginStationsCloseToPoint(chunk[chunk.length - 1]);
+        let maxLen = 0;
+        let maxCoordinates: any;
+        let maxIndex = 0;
+
+        
+        for (let j = 0; j < stationsFarEnd.length; j++) {
+          let station = stationsFarEnd[j]['AddressInfo'];
+          var pt = turf.point(
+            [station['Longitude'], station['Latitude']],
+            {
+              orderTime: Date.now(),
+              key: Math.random()
+            }
+          );
+          stations.push(pt);
+          
+          let geoJsonForLength = await this.getRouteWithPoints([chunk[0], [station['Longitude'], station['Latitude']]]);
+          let len = turf.length(geoJsonForLength);
+          
+          if (len < 500  && len > maxLen) {
+            maxLen = len;
+            maxCoordinates = [station['Longitude'], station['Latitude']];
+            maxIndex = j;
+          }
+        }
+        var pt = turf.point(
+          maxCoordinates,
+          {
+            orderTime: Date.now(),
+            key: Math.random()
+          }
+        );
+        stations.splice(maxIndex, 1);
+        usedStations.push(pt);
+        waypoints.push(maxCoordinates);
+      }
+      waypoints.push(to);
+      console.log(waypoints);
+      geoJson = await this.getRouteWithPoints(waypoints);
+    }
+
+    return [geoJson, stations, usedStations];
   }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -26,6 +26,7 @@ export class MapComponent implements AfterViewInit {
   
   dropoffs = turf.featureCollection([]);
   chargers = turf.featureCollection([]);
+  usedChargers = turf.featureCollection([]);
   detour = turf.featureCollection([]);
   
   
@@ -144,6 +145,7 @@ export class MapComponent implements AfterViewInit {
           'icon-allow-overlap': true,
           'icon-ignore-placement': true,
           'icon-image': 'marker-15',
+          'icon-size': 2
         }
       });
 
@@ -155,10 +157,30 @@ export class MapComponent implements AfterViewInit {
           data: this.chargers,
           type: 'geojson'
         },
+        paint: {
+          'text-color': '#ff0000'
+        },
         layout: {
           'icon-allow-overlap': true,
           'icon-ignore-placement': true,
           'icon-image': 'fuel-15',
+          'icon-size': 2          
+        }
+      });
+
+
+      map.addLayer({
+        id: 'used-chargers-symbol',
+        type: 'symbol',
+        source: {
+          data: this.usedChargers,
+          type: 'geojson'
+        },
+        layout: {
+          'icon-allow-overlap': true,
+          'icon-ignore-placement': true,
+          'icon-image': 'rocket-15',
+          'icon-size': 2          
         }
       });
     });
@@ -176,44 +198,6 @@ export class MapComponent implements AfterViewInit {
     return await this.http.get('https://api.openchargemap.io/v2/poi/?latitude=' + point[1] + '&longitude=' + point[0] + '&distance=300&maxresults=10').toPromise();
   }
   
-  //primeste array de array de coordonate => geojson
-  async getRouteWithPoints(points) {
-      let data = await this.http.get(this.assembleQueryURL(points)).toPromise();
-      // Create a GeoJSON feature collection
-      var routeGeoJSON = turf.featureCollection([turf.feature(data['trips'][0].geometry)]);
-      
-      // If there is no route provided, reset
-      if (!data['trips'][0]) {
-        routeGeoJSON = this.nothing;
-      }
-        // Update the `route` source by getting the route source
-        // and setting the data equal to routeGeoJSON
-        // var chunk = turf.lineChunk(routeGeoJSON, 15, {units: 'miles'});
-      
-      return routeGeoJSON;
-  }
-  
-  //afiseaza punctele clicked
-  pointsFromMap() {
-    let points: any[] = [[this.truckLocation.lng, this.truckLocation.lat]];
-    // Create an array of GeoJSON feature collections for each point
-    var restJobs = this.dropoffs.features;
-    
-    // If there are actually orders from this restaurant
-    if (restJobs.length > 0) {
-      
-      // If the request was made after picking up from the restaurant,
-      // Add the restaurant as an additional stop
-      
-      restJobs.forEach((d, i) => {
-        // Add dropoff to list
-        let k = d.geometry.coordinates;
-        points.push(k);
-      });
-    }
-    return points
-  }
-  
   //pune un punct pe map
   async newDropoff(coords) {
     // Store the clicked point as a new GeoJSON feature with
@@ -228,60 +212,15 @@ export class MapComponent implements AfterViewInit {
     );
     this.dropoffs.features.push(pt);
     
-    let data = await this.http.get(this.assembleQueryURL(this.pointsFromMap())).toPromise();
-      // Create a GeoJSON feature collection
-      
-      var routeGeoJSON = turf.featureCollection([turf.feature(data['trips'][0].geometry)]);
-      
-      // If there is no route provided, reset
-      if (!data['trips'][0]) {
-        routeGeoJSON = this.nothing;
-      } else {
-        // Update the `route` source by getting the route source
-        // and setting the data equal to routeGeoJSON
-        var chunk = turf.lineChunk(routeGeoJSON, 300, {units: 'kilometers'});
-        //cauta statiile in jurul far endului (doar pt primul chunk)
-        let stationsFarEnd:any = await this.getCharginStationsCloseToPoint(chunk.features[0].geometry['coordinates'][chunk.features[0].geometry['coordinates'].length - 1]);
+    let data = await this.mapService.makeRoad([this.truckLocation.lng, this.truckLocation.lat], [coords.lng, coords.lat]);
 
-        for (let i = 0; i < stationsFarEnd.length; i++) {
-          var pt = turf.point(
-            [stationsFarEnd[i]['AddressInfo']['Longitude'], stationsFarEnd[i]['AddressInfo']['Latitude']],
-            {
-              orderTime: Date.now(),
-              key: Math.random()
-            }
-          );
-          this.chargers.features.push(pt);
-          this.updateChargers(this.chargers);
-        }
-        // let routeGeoJSON2 = ;
-        let possibleRoutes = [];
-        let maxLen = 0;
-        let maxCoordinates: any;
-        for (let i = 0; i < stationsFarEnd.length ; i++) {
-          let geoJson = await this.getRouteWithPoints([chunk.features[0].geometry['coordinates'][0], [stationsFarEnd[i]['AddressInfo']['Longitude'], stationsFarEnd[i]['AddressInfo']['Latitude']]]);
-          let len = turf.length(geoJson);
-          console.log(len);
-
-          if (len < 500 && len > 200 && len > maxLen) {
-            console.log("DA BA PULA");
-            maxLen = len;
-            maxCoordinates = {lng: stationsFarEnd[i]['AddressInfo']['Longitude'], lat: stationsFarEnd[i]['AddressInfo']['Latitude']};
-            
-          } else {
-            console.log("Nu BA PULA");
-            
-          }
-        }
-        this.newDropoff(maxCoordinates);
-
-        
-        this.mapService.map.getSource('route2').setData(routeGeoJSON);
-      }
-      
-      if (data['waypoints'].length === 12) {
-        window.alert('Maximum number of points reached. Read more at mapbox.com/api-documentation/#optimization.');
-      }
+    console.log(data);
+    this.chargers.features = data[1];
+    this.usedChargers.features = data[2];
+    this.updateChargers(this.chargers);
+    this.updateUsedChargers(this.usedChargers);
+    this.mapService.map.getSource('route2').setData(data[0]);
+    
   }
   
   updateDropoffs(geojson) {
@@ -293,25 +232,11 @@ export class MapComponent implements AfterViewInit {
     this.mapService.map.getSource('chargers-symbol')
     .setData(geojson);
   }
-  
-  
-  
-  // Here you'll specify all the parameters necessary for requesting a response from the Optimization API
-  assembleQueryURL(points: any[]) {
-    
-    // Store the location of the truck in a variable called coordinates
-    var coordinates: any = points;
-    
-    // Set the profile to `driving`
-    // Coordinates will include the current location of the truck,
-    return 'https://api.mapbox.com/optimized-trips/v1/mapbox/driving/' + coordinates.join(';') + '?overview=full&roundtrip=false&source=first&destination=last&steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+
+  updateUsedChargers(geojson) {
+    this.mapService.map.getSource('used-chargers-symbol')
+    .setData(geojson);
   }
   
-  objectToArray(obj) {
-    var keys = Object.keys(obj);
-    var routeGeoJSON = keys.map(function(key) {
-      return obj[key];
-    });
-    return routeGeoJSON;
-  }
+  
 }
